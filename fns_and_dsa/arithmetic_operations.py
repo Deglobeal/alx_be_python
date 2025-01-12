a1f9d653968325bbc194b06973ee0f1c646ff4ad
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-01-12 18:58:32 +0100

    Update arithmetic_operations.py
    code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,3 +1,12 @@
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}") 
+    
 def perform_operation(num1, num2, operation):
     # Perform the operation based on the 'operation' argument
     if operation == 'add':
@@ -16,11 +25,3 @@ def perform_operation(num1, num2, operation):
         return "Error: Invalid operation. Please choose 'add', 'subtract', 'multiply', or 'divide'."
 
 
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
