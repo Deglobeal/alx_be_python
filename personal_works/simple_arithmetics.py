5a9219d5a7aa326bafb1e85fa781506623d6a1e5
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-01-19 23:48:14 +0100

    practices

---
+++ personal_works/simple_arithmetics.py
@@ -0,0 +1,26 @@
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    num3 = float(input("Enter the third number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+    result = perform_operation(num1, num2, num3, operation)
+    print(f"Result: {result}") 
+    
+def perform_operation(num1, num2, num3, operation):
+    # Perform the operation based on the 'operation' argument
+    if operation == 'add':
+        return num1 + num2 + num3
+    elif operation == 'subtract':
+        return num1 - num2 - num3
+    elif operation == 'multiply':
+        return num1 * num2 * num3
+    elif operation == 'divide':
+        # Handle division by zero
+        if num2 == 0 | num3 == 0 | num3 == 0:
+            return "Error: Division by zero is not allowed."
+        else:
+            return num1 / num2 / num3
+    else:
+        return "Error: Invalid operation. Please choose 'add', 'subtract', 'multiply', or 'divide'."
