ecab79c362fdb658d06d4553166ded3660222ee6
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-01-23 21:51:26 +0100

    corrected the work on book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -6,15 +6,14 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        print(f"Book '{self.title}' by {self.author} created!")
 
     def __del__(self):
         # Destructor that prints when a Book object is deleted
-        print(f"Deleting '{self.title}'")
+        print(f"Deleting {self.title}")
 
     def __str__(self):
         # String representation method for a user-friendly output
-        return f"'{self.title}' by {self.author}, published in {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
         # Official representation that can be used to recreate the object
@@ -23,14 +22,12 @@ class Book:
 
 # Example Usage (outside of the class)
 if __name__ == "__main__":
-    # Creating instances of Book
-    book1 = Book("Pride and Prejudice", "Jane Austen", 1813)
-    book2 = Book("1984", "George Orwell", 1949)
+    # Creating an instance of Book
+    book = Book("1984", "George Orwell", 1949)
 
     # Using __str__ and __repr__
-    print(book1)  # This calls __str__
-    print(repr(book2))  # This calls __repr__
+    print(book)  # This calls __str__
+    print(repr(book))  # This calls __repr__
 
-    # Deleting the objects to trigger __del__
-    del book1
-    del book2
+    # Deleting the object to trigger __del__
+    del book
